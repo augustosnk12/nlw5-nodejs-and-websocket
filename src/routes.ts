d7ad38bcba2b1307795ed663c453b3321cf20e47
augustosnk12 <augustosnk12@outlook.com>
2021-06-02 23:49:23 -0300

    add messages resource

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 
 import { SettingsController } from "./controllers/SettingsControllers";
 import { UsersController } from "./controllers/UsersController";
+import { MessagesController } from "./controllers/MessagesController";
 
 const routes = Router();
 
 const settingController = new SettingsController();
 const userController = new UsersController();
+const messageController = new MessagesController();
 
 routes.post("/settings", settingController.create);
+
 routes.post("/users", userController.create);
+
+routes.post("/messages", messageController.create);
+routes.get("/messages/:id", messageController.showByUser);
 
 export { routes };
---
+++ src/services/MessagesService.ts
@@ -0,0 +1,43 @@
+import { getCustomRepository } from "typeorm";
+import { MessagesRepository } from "../repositories/MessagesRepository";
+
+interface IMessageCreate {
+  admin_id?: string;
+  text: string;
+  user_id: string;
+}
+
+class MessageService {
+  /**
+   * Create a new message
+   */
+  async create({ admin_id, text, user_id }: IMessageCreate) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const message = messagesRepository.create({
+      admin_id,
+      text,
+      user_id,
+    });
+
+    await messagesRepository.save(message);
+
+    return message;
+  }
+
+  /**
+   * List messages by user_id
+   * @param user_id
+   */
+  async listByUser(user_id: string) {
+    const messagesRepository = getCustomRepository(MessagesRepository);
+
+    const list = await messagesRepository.find({
+      where: { user_id }, relations: ['user']
+    });
+
+    return list;
+  }
+}
+
+export { MessageService };
